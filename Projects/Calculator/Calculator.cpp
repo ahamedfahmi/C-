084ac6627902fe00c725bc3a5277a913e22eac31
Ahamed Fahmi <76228369+ahamedfahmi@users.noreply.github.com>
2021-07-21 10:52:23 +0000

    Changed Code entirely

--- Projects/Calculator/Calculator.cpp
+++ Projects/Calculator/Calculator.cpp
@@ -1,33 +1,46 @@
 #include <iostream>
 using namespace std;
 
+/*###############################
+  # This is a Simple Calculator # 
+#################################*/
 int main() {
     //define variables
-    float num1 , num2 , answer;
+    float num1 , num2 , answer=0;
     char sign;
 
     //prompt for user & get user input
-    cout<<"Enter the number 1 =";
+    cout<<"Enter the number 1 = ";
     cin>>num1;
 
-    cout<<"Enter the number 2 =";
+    cout<<"Enter the number 2 = ";
     cin>>num2;
 
-    cout<<"Enter the operator =";
+    cout<<"Enter the operator = ";
     cin>>sign;
 
-    //calculation & condition
-    if (sign == '+')
-        answer = num1 + num2;
-    else if (sign == '-')
-        answer = num1 - num2;
-    else if (sign == '/')
-        answer = num1 / num2;
-    else if (sign == '*')
-        answer = num1 * num2;
+    //calculation & condition & display output
+    switch(sign)
+    {
+      case '+':
+              cout<<"The answer is = "<<num1 + num2<<endl;
+              break;
 
-    //Display output
-    cout<<"The answer is = "<<answer<<endl;
+      case '-':
+              cout<<"The answer is = "<< num1 - num2<<endl;
+              break;
+
+      case '/':
+              cout<<"The answer is = "<<num1 / num2<<endl;
+              break;  
+      case '*':
+              cout<<"The answer is = "<<num1 * num2<<endl;
+              break;  
+
+      default:
+              cout<<"Invalid operator !!!";  
+              return 0;                                        
+    }
 
     return 0;
 }
